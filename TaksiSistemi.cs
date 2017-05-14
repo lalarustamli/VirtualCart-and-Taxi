b26bbc13dcca08552276ff229795ca30901f366f
Lala Rustamli <rustamlilala@gmail.com>
2017-05-14 16:38:33 +0400

    Add files via upload

---
+++ TaksiSistemi.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaksiSistemi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var op = new Operator();
+            Console.WriteLine("Bos taksilerin siyahisi: ");
+            op.TaxiGenerator();
+            op.CustomerInfo();
+            op.TaxiSelector();
+
+
+        }
+    }
+    public class Customer
+    {
+        public string name;
+        public double startX;
+        public double startY;
+        public double finishX;
+        public double finishY;
+        public double distance;
+        public Customer(string name, double startX, double startY, double finishX, double finishY,double distance)
+        {
+            this.name = name;
+            this.startX = startX;
+            this.startY = startY;
+            this.finishX = finishX;
+            this.finishY = finishY;
+            this.distance = distance;
+        }
+    }
+
+    public class Operator
+    {
+        public List<Taxi> taxiList = new List<Taxi>();
+        public List<Customer> customerList = new List<Customer>();
+
+        public void CustomerInfo()
+        {
+            Console.WriteLine("Adinizi daxil edin");
+            string name = Console.ReadLine();
+            Console.WriteLine("Oluqunuz mekanin X koordinatini daxil edin: ");
+            double stX = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Oluqunuz mekanin Y koordinatini daxil edin: ");
+            double stY = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Gedilecek mekanin X koordinatini daxil edin: ");
+            double finX = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Gedilecek mekanin Y koordinatini daxil edin: ");
+            double finY = Convert.ToDouble(Console.ReadLine());
+            double distance = Math.Pow((finX - stX), 2) + Math.Pow((finY - stY), 2);
+            Console.WriteLine("Gedeceyiniz mesafe {0}km",distance);
+            customerList.Add(new Customer(name, stX, stY, finX, finY,distance));
+           
+        }
+
+
+
+
+
+        public void TaxiGenerator()
+        {
+            taxiList.Add(new Taxi(101, 41, 49));
+            taxiList.Add(new Taxi(102, 35, 53));
+            taxiList.Add(new Taxi(103, 40, 55));
+            taxiList.Add(new Taxi(104, 43, 52));
+            taxiList.Add(new Taxi(104, 37, 50));
+            foreach (var taxi in taxiList)
+            {
+                Console.WriteLine("Masin nomresi: " + taxi.carNumber + " Hal-hazirki koordinatlari : " + taxi.pointX + " , " + taxi.pointY);
+            }
+
+        }
+
+        public void TaxiSelector()
+        {
+            
+            
+           
+            List<double> distList = new List<double>();
+            
+            
+            foreach (var taxi in taxiList)
+            {
+                double taxi2Customer = Math.Pow((taxi.pointX - customerList[0].startX), 2) + Math.Pow((taxi.pointY - customerList[0].startY), 2);
+                taxi.taxi2customer = taxi2Customer;
+                distList.Add(taxi.taxi2customer);
+                taxi.time2customer = taxi.velocity * taxi.taxi2customer;
+
+                
+                
+            }
+            distList.Sort();
+            
+            for (int i = 0; i < taxiList.Count; i++)
+            {
+                if (taxiList[i].taxi2customer == distList[0])
+                {
+                    Console.WriteLine(customerList[0].name+", size en yaxin taksinin masin nomresi: " + taxiList[i].carNumber + " Mesafe: " + taxiList[i].taxi2customer + " km " + taxiList[i].time2customer +" deqiqeye catacaq " + customerList[0].distance * 3 + " deqiqeye sizi mekana catdiracaq ");
+                    Console.WriteLine("Gedis haqqi {0} AZN", customerList[0].distance*2);
+                }
+            }
+            
+        }
+
+
+
+        public class Taxi
+        {
+            public int carNumber;
+            public double pointX;
+            public double pointY;
+            public double taxi2customer;
+            public double velocity = 3;
+            public double time2customer;
+            public double time4customer;
+            public Taxi(int carNumber, double pointX, double pointY)
+            {
+                this.carNumber = carNumber;
+                this.pointX = pointX;
+                this.pointY = pointY;
+            }
+        }
+
+    }
+
+}
---
+++ VirtualKart.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualKardSistemi
+{
+     class Program
+    {
+        enum Dates
+        {
+            halfyear = 6,
+            year = 12,
+            yearhalf = 18,
+            twoyears = 24
+        }
+
+
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("YarimKiloluqBanka xosh gelmisiniz!");
+            Console.WriteLine("Virtual kart acmaq ucun oz kartiniz olmalidir. Lutfen adinizi daxil edin:");
+            string ad = Console.ReadLine();
+            Console.WriteLine("Soyadinizi unutmayin :");           
+            string soyad = Console.ReadLine();           
+            Console.WriteLine("Kartiniza pul qoyun. Mebleqi daxil edin :");           
+            int pul = Convert.ToInt32(Console.ReadLine());          
+            Console.WriteLine("Kartdan neche ay istifade edeceksiniz?"); 
+            int ay = Convert.ToInt32(Console.ReadLine());
+            var card = new Card(ad, soyad, pul, ay); 
+            Console.WriteLine("Kartiniz hazirdir!");
+            Console.WriteLine("Istifadeçi adı : " + card.Name + "\n" + "İstifadeçi soyadı : " + card.Surname + "\nKart nomresi : "+card.CardNum + "\nCVC kodu: " + card.Cvc +"\nYararlidir : " + card.DeadlineInput+"\nKartdaki mebleq : " + card.Money ); 
+            Console.WriteLine("Virtual kart yaradacaqiniz shexsin adini daxil edin: ");     
+            string vAd = Console.ReadLine();
+            Console.WriteLine("Gondermek istediyiniz meqleqi daxil edin");
+            int vMebleq = Convert.ToInt32(Console.ReadLine());  
+            Console.WriteLine("Virtual kart ucun yararliliq muddeti neche ay olsun?");
+            int vAy = Convert.ToInt32(Console.ReadLine());
+            card.AddVirtualCard(vAd, vMebleq, vAy);
+            Console.WriteLine("Kartinizda qaliq mebleq : " + card.Money);
+            var vCard = card.GetVirtualCard(vAd);
+            Console.WriteLine("Virtual Kart hazirdir!");
+            Console.WriteLine("Virtual kart sahibinin adi :" + vCard.Login + "\nKart nomresi :" + vCard.Vcardnumber +"\nKartdaki mebleq : "+vCard.Money);
+        }
+    }
+
+    
+    public class Card
+    {
+
+        
+        private static string _name;
+        private static string _surname;
+        private static string _cardnumber;
+        private static string _deadlineInput;
+        private static string _cvc;
+        private static double _money;
+        private static DateTime _deadlineOutput;
+        private static List<Virtualcard> _vcards = new List<Virtualcard>();
+        public static Random rand = new Random();
+        
+
+
+        public string Name { get { return _name; } }
+        public string Surname { get { return _surname; } }
+        public string CardNum { get { return _cardnumber; } }
+        public string DeadlineInput { get { return _deadlineInput; } }
+        public string Cvc { get {return _cvc; } }
+        public double Money { get { return _money; } }
+
+       
+
+
+        private static string DeadlineDefiner(int arg)
+        {
+            DateTime deadline = DateTime.Now.AddMonths(arg);
+            _deadlineOutput = deadline;
+            return deadline.Month + " / " + deadline.Year.ToString().Substring(2); 
+        }
+
+        private static string GenerateCardNumber(int num)
+        {
+            string result = " ";
+            for (int i  = 0; i < num; i++)
+            {
+                result += rand.Next(1, 9);
+            }
+            return result;
+        }
+
+        public void AddVirtualCard(string login, double money, int month)
+        {
+            if (DateTime.Now.AddMonths(month) > _deadlineOutput || _money < money)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Error : Qaydalari pozdunuz./nQayda 1 : Virtual kartin yayarliliq muddeti real kartdan boyuk ola bilmez.\nQayda 2 : Virtual karta oturduyunuz mebleq real kartdan artiq ola bilmez. ");
+                Console.ResetColor();
+                
+            }
+            _money -= money;
+            _vcards.Add(new Virtualcard(login, money, month));
+        }
+        public Virtualcard GetVirtualCard(string login)
+        {
+            for (int i = 0; i < _vcards.Count; i++)
+            {
+                if (_vcards[i].Login == login)
+                {
+                    return _vcards[i];
+                }
+            }
+            return null;
+            
+        }
+
+
+        public Card(string name, string surname, double money, int date)
+        {
+            _name = name;
+            _surname = surname;
+            _money = money;
+            _cardnumber = GenerateCardNumber(16);
+            _cvc = GenerateCardNumber(3);
+            _deadlineInput = DeadlineDefiner(date);
+        }
+
+    }
+
+
+
+    //VirtualCard
+    public class Virtualcard
+    {
+        
+        private string _login;
+        private string __vCardNumber;
+        private DateTime _deadlineInput;
+        private double _money;
+        public static Random rand = new Random();
+
+
+        public string Login { get { return _login; } }
+        public string Vcardnumber { get { return __vCardNumber; } }
+        public double Money { get { return _money; } }
+        public string DeadlineInput { get { return _deadlineInput.Month + " / " + _deadlineInput.Year; } }
+
+
+        private string Getvirtualcardnumber(int num)
+        {
+            string result = "";
+            for (int i = 0; i < num; i++)
+            {
+                result += rand.Next(0, 9);
+            }
+            return result;
+        }
+
+        public Virtualcard(string login,double money, int month)
+        {
+            _login = login;
+           
+            _money = money;
+            _deadlineInput = DateTime.Now.AddMonths(month);
+            __vCardNumber = Getvirtualcardnumber(16);
+
+        }
+
+    }
+
+}
